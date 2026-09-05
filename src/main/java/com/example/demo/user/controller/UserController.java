89ee80fe3e91b6e2d475229afd3012fc252ba4bf
lazheart <176245760+Lazheart@users.noreply.github.com>
2026-09-05 13:33:55 -0500

    feat: User Endpoint username

--- src/main/java/com/example/demo/user/controller/UserController.java
+++ src/main/java/com/example/demo/user/controller/UserController.java
@@ -1,8 +1,18 @@
 package com.example.demo.user.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController 
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+
+    @GetMapping("/username/{username}")
+    public String getUserByUsername(String username) {
+        return "User: " + username;
+    };
+
     
 }
--- src/main/java/com/example/demo/user/repository/UserRepository.java
+++ src/main/java/com/example/demo/user/repository/UserRepository.java
@@ -4,6 +4,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.user.domain.User;
 
-public interface UserRepository extends JpaRepository<User, Long>{
-    
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID>{
+
+    User findByUsername(String username);
+    Optional<User> findById(UUID id);
+    Optional<User> findByEmail(String email);
+    List<User> findAllByAgeGreaterThanEqual(Integer age);
+
 }
